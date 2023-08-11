7dd39917a4c97836862835ddcd6214cd4526ec1b
GabrielYujiYasuda <gabrielyyasuda@gmail.com>
2023-08-11 11:25:06 -0300

    Using useState

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./List.module.scss";
 import Item from "./Item";
 
 export default function List() {
-  const tasks = [
+  const [tasks, setTasks] = useState([
     {
       task: "React",
       time: "02:00:00",
@@ -16,10 +16,18 @@ export default function List() {
       task: "C#",
       time: "05:30:12",
     },
-  ];
+  ]);
 
   return (
     <aside className={style.listaTarefas}>
+      <h2
+        onClick={() => {
+          console.log("CLICKED");
+          setTasks([...tasks, { task: "Study state", time: "05:00:00" }]);
+        }}
+      >
+        Daily studies
+      </h2>
       <ul className="">
         {tasks.map((t, i) => (
           <Item key="i" {...t} />
